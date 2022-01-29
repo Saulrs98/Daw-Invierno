a2482b599538c2f4be73ca559b8ff1f67261f09d
Saulrs98 <A01351663@itesm.mx>
2022-01-28 19:10:44 -0600

    Lab 9

--- Lab9/routes/routeindex.js
+++ Lab9/routes/routeindex.js
@@ -1,11 +1,13 @@
-
 const express = require('express');
 const router = express.Router();
-//const Task = require('../model/task');
+const Post = require('../model/post');
 
 
 router.get('/', async function(req,res){
-  res.render('index');
+
+  let posts = await Post.find()
+
+  res.render('index',{posts});
 });
 
 
@@ -15,12 +17,37 @@ router.get('/newPost', async (req,res) =>{
 
 router.post('/newPost', async (req,res) =>{
 
-  let task = new Task(req.body)
-  await  task.save()
-  res.redirect('/')
- });
+  let post = new Post(req.body)
+  await post.save()
+  res.redirect("/")
+});
 
 
+router.get('/edit/:id', async (req,res) =>{
+  let id = req.params.id
+  let post = await Post.findById(id)
+
+  res.render('edit',{post});
+});
+
+
+// # PENDIENTE
+router.post('/edit/:id', async (req,res) =>{
+
+  res.redirect("/")
+});
+
+
+router.get('/delete/:id', async (req,res) =>{
+  let id = req.params.id
+  let post = await Post.findById(id)
+
+  res.render('delete',{post});
+});
+
+
+
+module.exports = router;
 
 
 module.exports = router;
